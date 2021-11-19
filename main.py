ed14504723bdeb65eba6bdc946d3a6911c765961
Julio Cesar Martinez <julio-maca>
2021-11-19 14:58:34 -0600

    Se actualizo y se crea el archivo csv

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import argparse 
+import argparse
+import csv
+import datetime
 import logging
 logging.basicConfig(level=logging.INFO)
 import re
+
+from requests.exceptions import HTTPError, ContentDecodingError
+from urllib3.exceptions import MaxRetryError, DecodeError
+
 import news_page_objects as news
-
-from yaml.parser import Parser
-from requests.exceptions import HTTPError
-from urllib3.exceptions import MaxRetryError
-
-from common import config 
+from common import config
 
 logger = logging.getLogger(__name__)
 
 is_well_formed_link = re.compile(r'^https?://.+/.+$') # https://example.com/hello
-is_root_path = re.compile(r'^/.+$')     # some text
+is_root_path = re.compile(r'^/.+$')     # /hello
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
-    logging.info(f'Begining scrape for {host}')
+    logging.info(f'Beginning scrape for {host}')
     homepage = news.HomePage(news_site_uid, host)
 
     articles = []
@@ -26,13 +27,26 @@ def _news_scraper(news_site_uid):
         #print(link)
         article = _fetch_article(news_site_uid, host, link)
 
-
         if article:
-            logger.info('Article fetched!!!')
+            logger.info('Article fetched!!')
             articles.append(article)
-            print(article.title)
-    print(len(articles))
 
+
+    _save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = f'{news_site_uid}_{now}_articles.csv'
+
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_file_name, mode='w+', encoding='utf-8' ) as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
 
 def _fetch_article(news_site_uid, host, link):
     logger.info(f'Start fetching article at {link}')
@@ -41,12 +55,14 @@ def _fetch_article(news_site_uid, host, link):
 
     try:
         article = news.ArticlePage(news_site_uid, _build_link(host, link))
-    except (HTTPError, MaxRetryError) as e:
+    except (HTTPError, MaxRetryError, DecodeError, ContentDecodingError) as e:
         logger.warning('Error while fetching the article', exc_info=False)
 
     if article and not article.body:
         logger.warning('Invalid article. There is no body')
         return None
+
+    return article
 
 def _build_link(host, link):
     if is_well_formed_link.match(link):
@@ -55,7 +71,6 @@ def _build_link(host, link):
         return f'{host}{link}'
     else:
         return f'{host}/{link}'
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
